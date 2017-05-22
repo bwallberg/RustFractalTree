3e78d641001ce67f22dc7bb9c215dda9cf7a7406
Benjamin Wallberg <me@bwallberg.com>
2017-05-22 22:18:50 +0200

    Animate the fractal tree
    * Use the length of the branch to render it, not hardcoded 50.0
    
    Signed-off-by: Benjamin Wallberg <me@bwallberg.com>

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ struct Branch {
     start_y: f64,
     angle: f64,
     length: f64,
+    cur_length: f64,
     split: bool
 }
 
@@ -24,7 +25,8 @@ fn main() {
         start_x: width as f64 / 2.0,
         start_y: height as f64,
         angle: -90.0,
-        length: 150.0,
+        length: 100.0,
+        cur_length: 0.0,
         split: false
     });
 
@@ -41,8 +43,8 @@ fn main() {
                         [
                             branch.start_x,
                             branch.start_y,
-                            branch.start_x + (50.0 * branch.angle.to_radians().cos()),
-                            branch.start_y + (50.0 * branch.angle.to_radians().sin())
+                            branch.start_x + (branch.cur_length * branch.angle.to_radians().cos()),
+                            branch.start_y + (branch.cur_length * branch.angle.to_radians().sin())
                         ],
                         &c.draw_state, c.transform, g
                     );
@@ -50,33 +52,41 @@ fn main() {
             });
         }
 
-        if let Some(u) = e.update_args() {
+        if let Some(update_args) = e.update_args() {
             if depth > 0.0 {
                 let mut new_branches: Vec<Branch> = Vec::new();
+                let mut did_split = false;
                 for branch in branches.iter_mut() {
-                    if branch.split == false {
+                    if branch.split == false && branch.cur_length >= branch.length {
                         branch.split = true;
+                        did_split = true;
 
                         new_branches.push(Branch {
-                            start_x: branch.start_x + (50.0 * branch.angle.to_radians().cos()),
-                            start_y: branch.start_y + (50.0 * branch.angle.to_radians().sin()),
+                            start_x: branch.start_x + (branch.length * branch.angle.to_radians().cos()),
+                            start_y: branch.start_y + (branch.length * branch.angle.to_radians().sin()),
                             angle: branch.angle - 20.0,
-                            length: branch.length - 10.0,
+                            length: branch.length - 7.5,
+                            cur_length: 0.0,
                             split: false
                         });
 
                         new_branches.push(Branch {
-                            start_x: branch.start_x + (50.0 * branch.angle.to_radians().cos()),
-                            start_y: branch.start_y + (50.0 * branch.angle.to_radians().sin()),
+                            start_x: branch.start_x + (branch.length * branch.angle.to_radians().cos()),
+                            start_y: branch.start_y + (branch.length * branch.angle.to_radians().sin()),
                             angle: branch.angle + 20.0,
-                            length: branch.length - 10.0,
+                            length: branch.length - 7.5,
+                            cur_length: 0.0,
                             split: false
                         });
+                    } else if branch.cur_length < branch.length {
+                        branch.cur_length += 100.0 * update_args.dt;
                     }
                 }
 
-                branches.append(&mut new_branches);
-                depth -= 1.0;
+                if did_split {
+                    branches.append(&mut new_branches);
+                    depth -= 1.0;
+                }
             }
         }
     }
