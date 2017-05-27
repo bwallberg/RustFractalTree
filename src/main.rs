cda89aeba8b327c671d10ef6f7d3df1c53b20b5c
Benjamin Wallberg <me@bwallberg.com>
2017-05-27 21:55:59 +0200

    Added randomness to branch creation & changed the color to white

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-// TODO impl Branch to avoid unnecessary code duplication
-// TODO refactor 'depth' to actually be depth, currently it's just number of splits
 extern crate piston_window;
 extern crate rand;
 
 use piston_window::*;
-use rand::Rng; 
+use rand::Rng;
+
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+const ANGLE_MOD: f64 = 20.0;
 
 #[derive(Debug)]
 struct Branch {
@@ -16,24 +18,47 @@ struct Branch {
     split: bool
 }
 
+impl Branch {
+    fn new(start_x: f64, start_y: f64, angle: f64, length: f64) -> Branch {
+        Branch {
+            start_x: start_x,
+            start_y: start_y,
+            angle: angle,
+            length: length,
+
+            cur_length: 0.0,
+            split: false
+        }
+    }
+
+    fn get_end_x(&self) -> f64 {
+        self.start_x + self.cur_length * self.angle.to_radians().cos()
+    }
+
+    fn get_end_y(&self) -> f64 {
+        self.start_y + self.cur_length * self.angle.to_radians().sin()
+    }
+}
+
+fn get_random_length(start_length: f64) -> f64 {
+    start_length - rand::thread_rng().gen_range(5, 20) as f64
+}
+
+fn get_random_angle(start_angle: f64, direction: f64) -> f64 {
+    start_angle + direction * (ANGLE_MOD + (rand::thread_rng().gen_range(0, 10) as f64))
+}
+
 fn main() {
-    let width: u32 = 1280;
-    let height: u32 = 720;
-    let angle_mod: f64 = 20.0;
-    let mut depth: f64 = 100.0;
-
-    let mut window: PistonWindow = WindowSettings::new("RustFractalTree", [width,  height]).exit_on_esc(true).build().unwrap();
-
+    let mut window: PistonWindow = WindowSettings::new("RustFractalTree", [WIDTH, HEIGHT]).exit_on_esc(true).build().unwrap();
     let mut branches: Vec<Branch> = Vec::new();
+    let mut zero_reached = false;
 
-    branches.push(Branch {
-        start_x: width as f64 / 2.0,
-        start_y: height as f64,
-        angle: -90.0,
-        length: 100.0,
-        cur_length: 0.0,
-        split: false
-    });
+    branches.push(Branch::new(
+        WIDTH as f64 / 2.0,
+        HEIGHT as f64,
+        -90.0,
+        100.0
+    ));
 
     while let Some(e) = window.next() {
         if let Some(r) = e.render_args() {
@@ -48,8 +73,8 @@ fn main() {
                         [
                             branch.start_x,
                             branch.start_y,
-                            branch.start_x + (branch.cur_length * branch.angle.to_radians().cos()),
-                            branch.start_y + (branch.cur_length * branch.angle.to_radians().sin())
+                            branch.get_end_x(),
+                            branch.get_end_y()
                         ],
                         &c.draw_state, c.transform, g
                     );
@@ -58,49 +83,42 @@ fn main() {
         }
 
         if let Some(update_args) = e.update_args() {
-            if depth > 0.0 {
+            if zero_reached == false {
                 let mut new_branches: Vec<Branch> = Vec::new();
-                let mut did_split = false;
+                let mut nothing_updated = true;
                 for branch in branches.iter_mut() {
                     if branch.split == false && branch.cur_length >= branch.length {
+                        nothing_updated = false;
                         branch.split = true;
-                        did_split = true;
 
-                        let branch_one_length: f64 = branch.length - (rand::thread_rng().gen_range(5, 20) as f64);
-                        let branch_two_length: f64 = branch.length - (rand::thread_rng().gen_range(5, 20) as f64);
+                        let branch_one = Branch::new(
+                            branch.get_end_x(),
+                            branch.get_end_y(),
+                            get_random_angle(branch.angle, 1.0),
+                            get_random_length(branch.length)
+                        );
 
-                        let branch_one_angle: f64 = branch.angle - (angle_mod + (rand::thread_rng().gen_range(0, 10) as f64));
-                        let branch_two_angle: f64 = branch.angle + (angle_mod + (rand::thread_rng().gen_range(0, 10) as f64));
+                        let branch_two = Branch::new(
+                            branch.get_end_x(),
+                            branch.get_end_y(),
+                            get_random_angle(branch.angle, -1.0),
+                            get_random_length(branch.length)
+                        );
 
-                        if branch_one_length > 0.0 {
-                            new_branches.push(Branch {
-                                start_x: branch.start_x + (branch.length * branch.angle.to_radians().cos()),
-                                start_y: branch.start_y + (branch.length * branch.angle.to_radians().sin()),
-                                angle: branch_one_angle,
-                                length: branch_one_length,
-                                cur_length: 0.0,
-                                split: false
-                            });
+                        if branch_one.length > 0.0 {
+                            new_branches.push(branch_one);
                         }
-
-                        if branch_two_length > 0.0 {
-                            new_branches.push(Branch {
-                                start_x: branch.start_x + (branch.length * branch.angle.to_radians().cos()),
-                                start_y: branch.start_y + (branch.length * branch.angle.to_radians().sin()),
-                                angle: branch_two_angle,
-                                length: branch_two_length,
-                                cur_length: 0.0,
-                                split: false
-                            });
+                        if branch_two.length > 0.0 {
+                            new_branches.push(branch_two);
                         }
                     } else if branch.cur_length < branch.length {
+                        nothing_updated = false;
                         branch.cur_length += 100.0 * update_args.dt;
                     }
                 }
-
-                if did_split {
+                zero_reached = nothing_updated;
+                if new_branches.len() > 0 {
                     branches.append(&mut new_branches);
-                    depth -= 1.0;
                 }
             }
         }
